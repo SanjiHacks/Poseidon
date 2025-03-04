ce3349ac656a0fe33007058cf7f712a42fbbc588
SanjiHacks <aadityaraturi7@gmail.com>
2025-03-04 18:50:10 +0000

    update

--- templates/facebook/script.js
+++ templates/facebook/script.js
@@ -1,3 +1,3 @@
 document.querySelector('.create-account').addEventListener('click', function() {
-    alert('Create Account functionality is not implemented yet.');
+    window.location.href = 'https://www.facebook.com/r.php?entry_point=login';
 });
